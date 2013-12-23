90be5db09182e0a93c32ac82cee35c887ec8595e
Nikolay Chaban <oxidburn@gmail.com>
2013-12-23 21:32:38 +0200

    Initial Commit

---
+++ ExamplesProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExamplesProject
+//
+//  Created by Chaban Nikolay on 12/23/13.
+//  Copyright (c) 2013 On Sight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
